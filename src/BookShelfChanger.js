d4f547c0bef24a0d104e2953c24e826dc475b98d
Abdul Rauf <abdulraufmujahid@gmail.com>
2018-04-29 23:33:43 +0500

    fix: make shelf optional

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 class BookShelfChanger extends Component {
   static propTypes = {
-    shelf: PropTypes.string.isRequired,
+    shelf: PropTypes.string,
     onShelfChange : PropTypes.func.isRequired
   }
 
@@ -12,8 +12,8 @@ class BookShelfChanger extends Component {
 
     return (
       <div className="book-shelf-changer">
-        <select value={shelf} onChange={(event) => onShelfChange(event.target.value)}>
-          <option value="none" disabled>Move to...</option>
+        <select value={shelf?shelf:'none'} onChange={(event) => onShelfChange(event.target.value)}>
+          <option value="moveTo" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
