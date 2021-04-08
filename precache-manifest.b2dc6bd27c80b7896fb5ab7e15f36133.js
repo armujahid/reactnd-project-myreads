a361592a3b5ddb185e8d86f8708d393d0b0d3986
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-08 07:48:35 +0000

    Deploy armujahid/reactnd-project-myreads to github.com/armujahid/reactnd-project-myreads.git:gh-pages

---
+++ precache-manifest.b2dc6bd27c80b7896fb5ab7e15f36133.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54980c3e1c366d86a175ad61694f704f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d839563c93209c7284",
+    "url": "/static/css/main.5007b531.chunk.css"
+  },
+  {
+    "revision": "0e8ff6017cad069eeb69",
+    "url": "/static/js/2.b6d98f16.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/static/js/2.b6d98f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d839563c93209c7284",
+    "url": "/static/js/main.e9b012c4.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
